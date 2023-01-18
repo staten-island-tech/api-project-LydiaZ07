0ceda1a3d82b97a90a1f3be7c9d9c98173d9fc04
LydiaZ07 <zhulydia07@gmail.com>
2023-01-18 13:12:54 -0500

    l

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,13 @@
-const URL ="";
+  const URL ="https://api.adviceslip.com/advice";
+
+    async function getData(URL){
+        try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data.content);
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+    getData(URL);
